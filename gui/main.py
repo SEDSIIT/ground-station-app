7bfde3e660fad23fb4b3191568c59af3f0abca69
aero-error <mngrom852@gmail.com>
2022-01-27 15:31:29 -0600

    Added static plot and support multiple OS

--- gui/main.py
+++ gui/main.py
@@ -7,7 +7,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-This software is the ground station GUI software that willbe used to view and
+This software is the ground station GUI software that will be used to view and
 analyze flight data while also be able to configure the custom flight computer
 built by the students of SEDS@IIT.
 
@@ -16,8 +16,7 @@ minimal additional packages and easy to use for users unfamiliar with the
 software.
 
 TO DO:
-- make all files and data relative
-- fix the quit() define bug
+- fix bug on static plot need to move plot to see plotted data
 - have matplotlib plots appear in the gui window in quadrants
 - have a performance metric bar on the side of the GUI
 - be able to communicate with STM32F4 over USB (COM)
@@ -34,7 +33,7 @@ from matplotlib import style
 from matplotlib import pyplot as plt
 
 import tkinter as tk
-from tkinter import Canvas, Label, ttk
+from tkinter import TOP, Canvas, Label, ttk
 from tkinter.filedialog import askopenfilename
 
 from PIL import ImageTk, Image
@@ -42,22 +41,40 @@ from PIL import ImageTk, Image
 import pandas as pd
 import numpy as np
 
-import settings
 import os
+import sys
+
+import settings
 ### IMPORT END ###
 
 ### STYLING START ###
 LARGE_FONT = ("Verdona", 12)
 style.use("ggplot")
 
-live_figure = Figure(figsize=(5,5), dpi=100)
-live_figure_subplot = live_figure.add_subplot(111)
+live_plot = Figure(figsize=(5,5), dpi=100)
+live_plot_subplot = live_plot.add_subplot(111)
+
+
+static_plot = Figure(figsize=(5,5), dpi=100)
+static_plot_subplot = static_plot.add_subplot(111)
 
 
 ### STYLING END ###
 
 ### GLOBAL VARIABLES START ###
 PATH = os.path.dirname(__file__)
+if sys.platform == "linux" or sys.platform == "linux2":
+    PLATFORM = "linux"
+elif sys.platform == "darwin":
+    PLATFORM = "macOS"
+elif sys.platform == "win32":
+    PLATFORM = "windows"
+else:
+    print("WARNING: Unrecognized platform")
+    quit()
+PATH_DATAFILE = ''
+PATH_LIVEDATA = os.path.join(PATH, 'data', 'example_data.csv') # placeholder
+
 ### GLOBAL VARIABLES END ###
 
 ### CLASS START ###
@@ -67,7 +84,7 @@ class GSApp(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
                 
         container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand = True)
+        container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
@@ -147,7 +164,9 @@ class HomePage(tk.Frame):
         button3.place(relx=0.7, rely=0.2, anchor="n")
         
         # image
-        filepath_logo_nobg = os.path.join(PATH,"images\\SEDSIIT-logo_noBG.png")
+
+        filepath_logo_nobg = os.path.join(PATH, 'images', 'SEDSIIT-logo_noBG.png')
+
         render = ImageTk.PhotoImage(Image.open(filepath_logo_nobg))
         img = ttk.Label(self, image=render)
         img.image = render
@@ -162,11 +181,18 @@ class DataAnalysis(tk.Frame):
         label = ttk.Label(self, text="Data Analysis", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text="Home",
-                            command=lambda: controller.show_frame(HomePage))
-        button1.pack()
-        # plot
+        button_file_select = ttk.Button(self, text="Select File",
+                                    command=lambda: select_file())
+        button_file_select.pack(side=TOP)
 
+        # static plot
+        canvas = FigureCanvasTkAgg(static_plot, self)
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas.draw()
+
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
 class FCConfig(tk.Frame):
     def __init__(self, parent, controller):
@@ -190,9 +216,9 @@ class LiveFlight(tk.Frame):
         button1.pack()
 
         # Live Plot
-        canvas = FigureCanvasTkAgg(live_figure, self)
-        canvas.draw()
+        canvas = FigureCanvasTkAgg(live_plot, self)
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas.draw()
 
         toolbar = NavigationToolbar2Tk(canvas,self)
         toolbar.update()
@@ -228,21 +254,29 @@ def get_win_dimensions(root):
 
 # Used to animate a matplotlib figure
 def animate_live_plot(i):
-    file_path_historical= os.path.join(PATH, 'data\\example_data.csv')
-
-    data_historical = pd.read_csv(file_path_historical)
-    data_historical.drop(["Events"], axis=1)
+    data = pd.read_csv(PATH_LIVEDATA)
+    data.drop(["Events"], axis=1)
     
-    live_figure_subplot.clear()
-    live_figure_subplot.plot(data_historical['Time'], data_historical['Altitude'], color="k")
-    live_figure_subplot.plot(data_historical['Time'], data_historical['Velocity'], color="r")
-    live_figure_subplot.set_xlabel("Time (sec)")
-    live_figure_subplot.set_ylabel("Height (ft)")
+    live_plot_subplot.clear()
+    live_plot_subplot.plot(data['Time'], data['Altitude'], color='k')
+    live_plot_subplot.plot(data['Time'], data['Velocity'], color='r')
+    live_plot_subplot.set_xlabel("Time (sec)")
+    live_plot_subplot.set_ylabel("AGL Altitude (ft)")
 
+def plot_static(): 
+    data = pd.read_csv(PATH_DATAFILE)
+    data.drop(["Events"], axis=1)
 
+    static_plot_subplot.clear()
+    static_plot_subplot.plot(data['Time'], data['Altitude'], color='k')
+    static_plot_subplot.set_xlabel("Time (sec)")
+    static_plot_subplot.set_ylabel("Altitude (ft)")
 
 def select_file():
-    file_dir = askopenfilename()
+    global PATH_DATAFILE
+    PATH_DATAFILE = askopenfilename()
+    print("Selected data file path: %s" % (PATH_DATAFILE))
+    plot_static()
 
 ### MAIN START ###
 def main():
@@ -257,10 +291,10 @@ def main():
     app.minsize(600,400)
     app.title("Ground Station Application")
 
-    filepath_icon_photo = os.path.join(PATH, 'images\\SEDSIIT-logo.png')
+    filepath_icon_photo = os.path.join(PATH, 'images', 'SEDSIIT-logo.png')
     app.tk.call('wm','iconphoto',app._w,tk.Image("photo", file=filepath_icon_photo))
 
-    ani = animation.FuncAnimation(live_figure, animate_live_plot, interval=500)
+    ani = animation.FuncAnimation(live_plot, animate_live_plot, interval=500)
 
     app.mainloop()
 ### MAIN END ###
