bda397d4b70a00be56bf14bee42e11002dbc2d6f
Austin Roddy <austinleeroddy@gmail.com>
2022-02-21 15:08:33 -0600

    Adding-Examples
    Uploading-examples

---
+++ Code-Examples/Basic-Communication-Drivers/PyCom.py
@@ -0,0 +1,27 @@
+import serial
+from serial import Serial
+
+ser = Serial('COM4', 115200)
+if not ser.isOpen():
+    ser.open()
+print('COM4 is open', ser.isOpen())
+
+
+while True:
+    action = input("r=read,  w=write,  q=quit: ")
+
+    if(action == 'r'):
+        print("r")
+        for i in range(10):
+            data = ser.readline(1000)
+            print(data)
+    elif(action == 'w'):
+        print("w")
+        send = "Send Data not "
+        for i in range(10):
+            ser.write( send.encode())
+            data = ser.readline(1000)
+            print(data)
+    elif(action == 'q'):
+        print("q, byebye!")
+        exit()
