72d73781daf10911d88f83695569cc0541c4678b
aero-error <mngrom852@gmail.com>
2022-03-13 22:57:01 -0500

    Split "main.py" into multiple files

---
+++ gui/pages/DataAnalysis.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+
+import lib.app_settings as settings
+import pages.HomePage
+import lib.plotting
+import lib.files
+
+
+class DataAnalysis(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        tk.Grid.rowconfigure(self, (0,5), weight=1) 
+        tk.Grid.columnconfigure(self, (0,4), weight=1) 
+        
+        label = ttk.Label(self, text="Data Analysis", font=settings.LARGE_FONT)
+        label.grid(column=2,row=0, sticky=tk.N)
+        
+        button_home = ttk.Button(self, text="Home",
+                                    command=lambda: controller.show_frame(pages.HomePage.HomePage))
+        button_home.grid(column=1,row=1)
+
+        button_file_select = ttk.Button(self, text="Open File",
+                                    command=lambda: lib.files.select_file())
+        button_file_select.grid(column=3, row=1)
+        
+        # static plot
+        canvas = FigureCanvasTkAgg(lib.plotting.static_plot, self)
+        canvas.get_tk_widget().grid(column=0, row=5, columnspan=5, rowspan=1, sticky="NSEW")
+        canvas.draw()
+        
+        toolbarFrame = tk.Frame(self)
+        toolbarFrame.grid(column=0, row=4, columnspan=5, sticky=tk.W)
+        toolbar = NavigationToolbar2Tk(canvas, toolbarFrame)
+        toolbar.update()
+        
+        # Static table
+        canvas2 = FigureCanvasTkAgg(lib.plotting.static_table, self)
+        canvas2.get_tk_widget().grid(column=0, row=3, columnspan=5, rowspan=1, sticky="NSEW")
+        canvas2.draw()
+        
+        toolbarFrame2 = tk.Frame(self)
+        toolbarFrame2.grid(column=0, row=2, columnspan=5, sticky=tk.W)
+        toolbar2 = NavigationToolbar2Tk(canvas2, toolbarFrame2)
+        toolbar2.update()
---
+++ gui/pages/core.py
@@ -0,0 +1,92 @@
+'''
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ABOUT:
+This defines the main window of the GUI. This acts as the tree trunk where
+the individual pages act as branches.
+'''
+
+import tkinter as tk
+import lib.app_settings as settings
+import lib.files
+import pages.HomePage
+import pages.DataAnalysis
+import pages.FCSettings
+import pages.Settings
+import pages.Telemetry
+
+class GSApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
+                
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        menubar = tk.Menu(container)
+        
+        # File Menu 
+        fileMenu = tk.Menu(menubar, tearoff=0)
+        fileMenu.add_command(label="Save Settings", command = lambda: tk.messagebox.showinfo("Information","Not supported yet!"))
+        fileMenu.add_command(label="Open", command = lambda: lib.files.select_file())
+        fileMenu.add_separator()
+        fileMenu.add_command(label="Exit", command = lambda: quit()) # Fixed?
+        menubar.add_cascade(label="File", menu=fileMenu)
+
+        # Page Menu
+        pageMenu = tk.Menu(menubar, tearoff=0)
+        pageMenu.add_command(label="Home", command = lambda: self.show_frame(pages.HomePage.HomePage))
+        pageMenu.add_separator()
+        pageMenu.add_command(label="Data Analysis", command = lambda: self.show_frame(pages.DataAnalysis.DataAnalysis))
+        pageMenu.add_command(label="FC Settings", command = lambda: self.show_frame(pages.FCSettings.FCSettings))
+        pageMenu.add_command(label="Live Flight Data", command = lambda: self.show_frame(pages.Telemetry.Telemetry))
+        menubar.add_cascade(label="Page", menu=pageMenu)
+
+        # Settings Menu
+        settingsMenu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Settings", menu=settingsMenu)
+
+        # Help Menu
+        helpMenu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Help", menu=helpMenu)
+
+        tk.Tk.config(self, menu=menubar)
+
+        self.frames = {}
+
+        # Load all pages initially
+        for page in (pages.HomePage.HomePage, pages.DataAnalysis.DataAnalysis, pages.FCSettings.FCSettings, pages.Telemetry.Telemetry, pages.Settings.Settings):
+
+            frame = page(container, self)
+
+            self.frames[page] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+        
+        self.show_frame(pages.HomePage.HomePage)
+
+    # Show frame that is requested
+    def show_frame(self, cont):
+        global CURRENT_PAGE
+        if (cont.__name__ == 'DataAnalysis'):
+            CURRENT_PAGE = "DataAnalysis"
+        elif (cont.__name__ == 'FCSettings'):
+            CURRENT_PAGE = "FCSettings"
+        elif (cont.__name__ == 'Telemetry'):
+            CURRENT_PAGE = "Telemetry"
+        elif (cont.__name__ == 'Settings'):
+            CURRENT_PAGE = "Settings"
+        else:
+            CURRENT_PAGE = "HomePage" 
+        if (settings.DEBUG.status == True):
+            print("CURRENT_PAGE: %s" %(CURRENT_PAGE))
+        frame = self.frames[cont]
+        frame.tkraise()
