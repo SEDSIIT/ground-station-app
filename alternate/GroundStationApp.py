f6ec7399d4fb49436dffecd92a753724878677ff
Marc103 <78170299+Marc103@users.noreply.github.com>
2024-02-14 19:35:54 -0600

    Added frame structure to gs app

--- alternate/GroundStationApp.py
+++ alternate/GroundStationApp.py
@@ -9,13 +9,15 @@ class GroundStationApp:
         # Control Panel window
         self.root = Tk()
         self.root.title("Control Panel")
-        self.root_mainframe = ttk.Frame(self.root)
+        self.root_mainframe = ttk.Frame(self.root, width=1920, height=1080)
+        self.root_mainframe.grid(column = 0, row = 0, sticky=(N, W, S, E))
 
         # Subroot toplevel window
         # Monitor window
         self.subroot = Toplevel(self.root)
         self.subroot.title("Monitor")
-        self.subroot_mainframe = ttk.Frame(self.root)
+        self.subroot_mainframe = ttk.Frame(self.subroot, width=1920, height=1080)
+        self.subroot_mainframe.grid(column = 0, row = 0, sticky=(N, W, S, E))
 
         # Control Panel window widget instantiations
         lm = self.live_metrics()
@@ -39,14 +41,26 @@ class GroundStationApp:
     # Control Panel window widget instantiations
     def live_metrics(self):
         frame = ttk.Frame(self.root_mainframe)
+        frame['width'] = 250
+        frame['height'] = 250
+        frame['borderwidth'] = 10
+        frame['relief'] = 'flat'
         return frame
     
     def connection_status(self):
         frame = ttk.Frame(self.root_mainframe)
+        frame['width'] = 250
+        frame['height'] = 250
+        frame['borderwidth'] = 10
+        frame['relief'] = 'sunken'
         return frame
     
     def nova_configuration(self):
         frame = ttk.Frame(self.root_mainframe)
+        frame['width'] = 250
+        frame['height'] = 250
+        frame['borderwidth'] = 10
+        frame['relief'] = 'groove'
         return frame
     
     def run(self):
